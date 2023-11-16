5220daf7bb27d6444c65338a6e4a9c5239c4e7f5
Gil Poiares-Oliveira <gpo@biodata.pt>
2023-11-16 15:44:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# BioData.pt Theme for Drupal
+
+Maintainer: [Gil Poiares-Oliveira](gpo@biodata.pt) (INESC-ID/BioData.pt)
+
+All BioData.pt logos and imagery are © Associação BIP4DAB - all rights reserved.
+
+Code is available under a GPL 2.0 license.
---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Behaviors for the BioData.pt theme.
+ */
+
+(function ($, Drupal) {
+  Drupal.behaviors.biodataptBS5 = {
+    attach() {
+      // BioData.pt JavaScript behaviors goes here.
+    },
+  };
+})(window.jQuery, window.Drupal);
